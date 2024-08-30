68d20564744eb542306507d057c9134ca3e7bf50
Martin Gjorgjiev <martin.gorgiev@rocketmail.com>
2024-08-30 17:07:34 +0200

    merge to public

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/pc_parts_assembler_app/public/';
+    public string $baseURL = 'http://localhost/pc_assembler/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
